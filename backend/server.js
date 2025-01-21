34304a915bb989c22af6d41b7f0277046e65e01f
Raiyan Wasi Siddiky <raiyanwasisiddiky@gmail.com>
2025-01-21 21:17:51 +0600

    temp commit  by raiyan

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,10 @@
 const express = require('express')
 
+// Sessions, morgan for HTTP requests, mongooes for MongoDB
+// const session = require('express-session');
+// const morgan = require('morgan');
+// const mongoose = require('mongoose');
+
 // express app
 const app = express();
 
@@ -12,3 +17,45 @@ app.get('/', (req, res) => {
 app.listen(4000, () => {
     console.log("Listening in PORT:4000");
 })
+
+// mongoose.connect(dbURI)
+//   .then((result)=>{
+//     app.listen(3000, ()=>{
+//         console.log("Server is running on port 3000");
+//     });
+//   })
+//   .catch((err)=>{
+//     console.log(err);
+//   });
+
+
+// STATIC FILES FOR IMAGE UPLOADS
+// app.use(morgan('dev'));
+// app.use(express.urlencoded({ extended:true }));
+// app.use(express.static('public'));
+// app.use('/uploads', express.static('uploads'));
+
+// app.use(express.json());
+
+// app.use(session({
+//   secret: 'boring  company',
+//   resave: false,
+//   saveUninitialized: true
+// }));
+
+// // Define error handling middleware
+// function sessionLogout(err, req, res, next) {
+//   if (err.message && err.message.includes("Cannot read properties of undefined (reading 'username')")) {
+//       // Redirect to the login page
+//       return res.redirect('/login');
+//   }
+
+//   // For other errors, proceed to the next middleware
+//   next(err);
+// }
+
+// app.use(sessionLogout);
+
+// app.use((req, res)=>{
+//     res.status(404).render('404', { title:"404" });
+// });
