d35fd9866cf828e98009506ea3ef6444d6eb4a91
trk345 <tawhidkhan3@gmail.com>
2025-04-22 16:48:57 +0600

    Set up vitest in project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { configDefaults } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,6 +9,15 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    exclude: [...configDefaults.exclude, 'dist'],
+    coverage: {
+      reporter: ['text', 'html'], // Generates terminal + HTML reports
     },
   },
 })
