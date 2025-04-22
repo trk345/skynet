97aacb4388116db5bc0fe1bf1372cac7fe31dfbf
trk345 <tawhidkhan3@gmail.com>
2025-04-22 22:43:27 +0600

    Remove frontend production connection lines from server.js

--- backend/server.js
+++ backend/server.js
@@ -189,13 +189,13 @@ app.use('/api/admin', authenticateJWT, adminRoutes); // Protected
 app.use('/api/user', authenticateJWT, userRoutes);  // Protected
 app.use('/api/vendor', authenticateJWT, vendorRoutes); // Protected
 
-// Serve static files
-app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
+// // Serve static files
+// app.use(express.static(path.join(__dirname, '..', 'frontend', 'dist')));
 
-// Catch-all route for React
-app.get('*', limiter, (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
-});
+// // Catch-all route for React
+// app.get('*', limiter, (req, res) => {
+//   res.sendFile(path.join(__dirname, '..', 'frontend', 'dist', 'index.html'));
+// });
 
 if (!process.env.MONGO_URI) {
   console.error("❌ MONGO_URI is missing in .env file!");
