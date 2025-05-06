23339e30a0e8ab7d66d8f643abf750190c0f4065
Tawhidur Rahman Khan <142532975+trk345@users.noreply.github.com>
2025-05-06 19:09:56 +0600

    Update logout controller to work in different domain production

--- backend/controllers/unlogControllers.js
+++ backend/controllers/unlogControllers.js
@@ -193,7 +193,7 @@ const authMe = async (req, res) => {
 
 
 const logout = (req, res) => {
-  res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: "Lax" });
+  res.clearCookie("token", { httpOnly: true, secure: process.env.NODE_ENV === 'production', sameSite: "None" });
   return res.status(200).json({ message: "Logout successful" });
 };
 
